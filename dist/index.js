1eaa7b34b23bddb81e72f8e656acfc1efba46721
Yessid Acosta <yesid9523@gmail.com>
2022-12-14 00:16:44 -0500

    Estudio de tipado de TS

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
 let mensaje = "Hola mundo";
-mensaje = "Hola pachito";
+mensaje = "Cadena ha cambiado por esta que lees";
+mensaje = "He cambiado nuevamente";
+console.log(mensaje);
+let cadenaInferida = "Soy una cadena automáticamente inferida";
+let numeroInferido = 34342233;
+let booleanoInferido = true;
+let algoInferido;
+let cadenaExplicitaNoInicializada;
+let cadenaExplicita = "Soy una cadena declarada explicitamente e inicializada";
+let cadenas = ["Lorem", "Ipsum", "Sit"];
+let numeros = [2, 4, 5];
+let arregloDeBooleanos = [true, false, true];
+numeros.map(x => x.toExponential);
+cadenas.map(x => x.length);
+let tupla = [1, 'Cadena'];
+let tuplaConArray = [1, ['Array', 'dentro', "De la tupla"]];
+var Talla;
+(function (Talla) {
+    Talla["Casado"] = "C";
+    Talla["Soltero"] = "S";
+    Talla["UnionLibre"] = "U";
+})(Talla || (Talla = {}));
+const estado = "CO";
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,102 @@
 let mensaje: String = "Hola mundo"
 
-mensaje = "Hola pachito"
+mensaje = "Cadena ha cambiado por esta que lees"
+
+mensaje = "He cambiado nuevamente"
+
+console.log(mensaje)
+
+/*Inferencia de TS*/
+let cadenaInferida = "Soy una cadena automáticamente inferida"
+let numeroInferido = 34_342_233;
+let booleanoInferido = true;
+
+let algoInferido;
+
+/*Declaración explicita */
+let cadenaExplicitaNoInicializada: string;
+let cadenaExplicita: string = "Soy una cadena declarada explicitamente e inicializada"
+
+
+/*Declaración de Arregloa */
+let cadenas =  ["Lorem", "Ipsum", "Sit"]; /*Array de strings inferidas*/
+let numeros: number[] = [2, 4, 5]; /*Array de numero explicito */
+
+let arregloDeBooleanos: Array<Boolean> = [true, false, true]; /*Array declarado directamente con la Interfaz*/
+
+
+
+/*Uso de métodos exlusivos para tipo de dato
+TS reconoce los métodos para cada variable y los sugiere
+*/
+
+numeros.map(x => x.toExponential);
+cadenas.map(x => x.length);
+
+
+/*Tuplas 
+Son colecciones definidas que pueden contener objatos de distintos tipos de datos siempre que cumplan con el orden y tipado declarado
+*/
+
+let tupla: [number, string] = [1, 'Cadena']
+let tuplaConArray: [number, string[]] = [1, ['Array', 'dentro', "De la tupla"]];
+
+/*Enums
+Tipo numerado sirve para relacionar pares Palabra: Abreviación. Util para almacenar estados o abreviacioens de cosas como Tallas de ropa, Opciones de datos. ETC
+Se declara así:
+
+enum Nombre { CosaUno, CosaDos, CosaTres } ---> Automáticamente sus equivalentes se asignan 0, 1, 2
+
+Ejemplo de posibles estados de una persona
+*/
+
+enum Talla { Casado = 'C', Soltero = 'S' , UnionLibre = 'U'}
+
+/*
+const
+Genera código en index.js cuando se le asigna valores a las constantes
+*/
+
+const enum Pais {
+    Colombia = 'CO', Ecuador = 'EC', Brasil = 'BR'
+}
+const estado = Pais.Colombia;
+
+/*Objetos 
+
+Objeto literal
+*/
+const objeto: {
+    readonly id: number, /*Solo lectura*/
+    nombre: string,
+    apellido?: string /*Opcional */
+    pais: Pais
+} = {id: 2, nombre: "David", pais: Pais.Ecuador}
+
+/*Declaración de Tipo type
+Util para declarar la estructura de un objeto y reutilizarla
+*/
+type Direccion = {
+    calle: string,
+    ciudad: string
+    pais: Pais
+}
+
+type Ciudadano = {
+    readonly id: number, /*Solo lectura*/
+    nombre: string,
+    apellido?: string /*Opcional */
+    direccion: Direccion
+}
+
+/*Objeto hecho apartir de type Ciudadano */
+const Camila: Ciudadano = {
+    id: 3,
+    nombre: "Camila",
+    direccion: {
+        calle: 'Avenida Brasil',
+        ciudad: 'Brasilia',
+        pais: Pais.Brasil
+    }
+}
+
